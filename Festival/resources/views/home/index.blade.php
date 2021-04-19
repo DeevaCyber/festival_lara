7a85fbacb791a3627d993b92aad7c39dd7a9d885
|Deeva| <i.maganga@yahoo.com>
2021-04-19 08:33:39 -0700

    Petite maj

--- Festival/resources/views/home/index.blade.php
+++ Festival/resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
 <h3 align='center'>Bienvenue sur l'application d'hébergement</h3>
   <p align='center' class='intro'>
     <img src="{{asset('images/sport.png')}}" alt="Logo du site" id="Logo" align="center"/><br>
-        La Maison des Ligues organise un festival de sports nature en spetembre, le festival Sp'Or.<br>
+        La Maison des Ligues organise un festival de sports nature en septembre, le festival Sp'Or.<br>
   </p>
 
 @include('include.fin')
